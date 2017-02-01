749f7428f78e7bfb7889e8ef52a4d2320dd4dfef
AlejandroReBa <reyes.bautista.alejandro@gmail.com>
2017-02-01 14:50:59 +0000

    User can now set coordinates to center the map. Errors about lack of one of the fields are handled.

--- app/src/main/java/com/example/a3reyea63/mapping/HelloMap.java
+++ app/src/main/java/com/example/a3reyea63/mapping/HelloMap.java
@@ -4,12 +4,16 @@ package com.example.a3reyea63.mapping;
 import android.app.Activity;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import org.osmdroid.config.Configuration;
 import org.osmdroid.views.MapView;
 import org.osmdroid.util.GeoPoint;
 
-public class HelloMap extends Activity {
+public class HelloMap extends Activity implements View.OnClickListener{
 
     MapView mv;
 
@@ -28,5 +32,25 @@ public class HelloMap extends Activity {
         mv.setBuiltInZoomControls(true);
         mv.getController().setZoom(14);
         mv.getController().setCenter(new GeoPoint(40.1, 22.5));//Litochoro, Greece
+
+        Button changeButton = (Button)findViewById(R.id.changeButton);
+        changeButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        EditText lat = (EditText)findViewById(R.id.editlat);
+        EditText lon = (EditText)findViewById(R.id.editlon);
+
+        TextView error = (TextView)findViewById(R.id.errorId);
+
+        if (lat.getText().toString().equals("") || lon.getText().toString().equals("")){
+            error.setText("ERROR: Please fill both longitude and latitude fields");
+        }else{
+            double doubleLat = Double.parseDouble(lat.getText().toString());
+            double doubleLon = Double.parseDouble(lon.getText().toString());
+            error.setText("");
+            mv.getController().setCenter(new GeoPoint(doubleLat, doubleLon));
+        }
     }
 }
